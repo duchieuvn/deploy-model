e6ef445523ea3e601561b6fe2da241a61b09063d
duchieuvn <tranthaiduchieu@gmail.com>
2022-08-25 14:29:31 +0700

    init

---
+++ static/script.js
@@ -0,0 +1,16 @@
+console.log('script')
+
+const image_input = document.querySelector("#image_input")
+var uploaded_image = ""
+
+
+image_input.addEventListener("change", () => {
+    const reader = new FileReader()
+    reader.addEventListener("load", ()=>{
+        uploaded_image = reader.result
+        document.querySelector("#display_image").style.backgroundImage = 'url(${uploaded_image})'
+
+    })
+
+    reader.readAsDataURL(this.files[0])
+})
